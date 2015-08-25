0887bb08bc65391cd05b087d6b729ed6ef137746
klocey <ken@weecology.org>
2015-08-25 08:18:29 -0400

    added and modified Aim3 document 2015_09_Aim3.Rmd; added new figure file Zvalues.png; modified SAR.R and metrics.R -KL

--- Aim3/bin/metrics.R
+++ Aim3/bin/metrics.R
@@ -20,3 +20,15 @@
 #### A function to generate observed richness
 S.obs <- function(x = ""){ rowSums(x > 0) * 1}
 
+sp.turnover <- function(site1, site2){
+  site1 <- site1[is.nan(site1) == FALSE]
+  site1 <- site1[site1 != '']
+  site2 <- site2[is.nan(site2) == FALSE]
+  site2 <- site2[site2 != '']
+  gamma = union(site1, site2)         # Gamma species pool
+  s     = length(gamma)                                   # Gamma richness
+  a.bar = mean(c(length(site1), length(site2)))   # Mean sample richness
+  b.w   = round(s/a.bar - 1, 4)
+  return(b.w)
+}
+
